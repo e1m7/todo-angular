73447a9983a78b8f78f2cad0e85a5e4ea13939c6
e1m7 <91698337+e1m7@users.noreply.github.com>
2021-10-10 17:36:37 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title = "Angular";
+  list: any[] = [];
+
+  addTask(item: string) {
+    this.list.push({
+      id: this.list.length,
+      name: item
+    });
+    console.warn(this.list);
+  }
+
+  removeTask(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+  }
+}
